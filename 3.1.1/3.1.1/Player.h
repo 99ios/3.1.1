e56115bd78d15c34c6df3e4188533ee18a841348
test <422538722@qq.com>
2017-03-28 17:15:22 +0800

    3.1.1

---
+++ 3.1.1/3.1.1/Player.h
@@ -0,0 +1,16 @@
+//
+//  Player.h
+//  3.1.1
+//
+//  Created by 李维佳 on 2017/3/22.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@interface Player : NSObject
+@property (nonatomic,assign) int healthPoint;
+@property (nonatomic,assign) int magicPoint;
++(Player *)player;
+-(void) normalAttack;
+-(void) magicAttack;
+@end
